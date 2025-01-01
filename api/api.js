4169f6cd743388b5d73a62d75db45229e76e4472
Ayesha Hussain <127660896+AyeshaHussain475@users.noreply.github.com>
2025-01-01 22:10:03 +0500

    add token in axios headers

--- api/api.js
+++ api/api.js
@@ -1,7 +1,19 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.106:7000/mini",
+  baseURL: "http://192.168.100.16:7000/mini",
 });
+
+(async () => {
+  try {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      api.defaults.headers.common["Authorization"] = "Bearer " + token;
+    }
+  } catch (error) {
+    console.error("Failed to retrieve token from AsyncStorage:", error);
+  }
+})();
 
 export default api;
