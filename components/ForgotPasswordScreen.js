715224e8529e78e691c4de322b798eb79fdc30e9
Ayesha Hussain <127660896+AyeshaHussain475@users.noreply.github.com>
2025-01-01 22:38:42 +0500

    update forgot screen

--- components/ForgotPasswordScreen.js
+++ components/ForgotPasswordScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -9,47 +9,52 @@ import {
   KeyboardAvoidingView,
   ScrollView,
   Platform,
-} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import api from '../api/api';
-import * as Animatable from 'react-native-animatable';
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import api from "../api/api";
+import * as Animatable from "react-native-animatable";
 
 const ForgotPasswordScreen = () => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const navigation = useNavigation();
 
   const handleSendOTP = async () => {
     if (!email) {
-      Alert.alert('Oops!', 'Please enter your email address');
+      Alert.alert("Oops!", "Please enter your email address");
       return;
     }
 
     try {
-      const response = await api.post('/auth/forgot-password', { email });
+      const response = await api.post("/reset-password", { email });
       if (response.status === 200) {
-        Alert.alert('Great!', 'A reset code has been sent to your email!');
-        navigation.navigate('OTPScreen', { email });
+        Alert.alert(
+          "Great!",
+          "Reset Password email has been sent successfully! Please check your email"
+        );
+        // navigation.navigate('OTPScreen', { email });
       }
     } catch (error) {
       console.error(error);
-      Alert.alert('Uh-oh!', 'Failed to send the OTP. Please try again.');
+      Alert.alert("Uh-oh!", "Failed to send the email. Please try again.");
     }
   };
 
   return (
     <KeyboardAvoidingView
       style={styles.container}
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         <Animatable.Image
           animation="bounceIn"
           duration={1500}
-          source={require('../assets/logo1.jpg')}
+          source={require("../assets/logo1.jpg")}
           style={styles.logo}
         />
         <Text style={styles.title}>Forgot Password?</Text>
-        <Text style={styles.subtitle}>We’ll send you a magic code to reset it!</Text>
+        {/* <Text style={styles.subtitle}>
+          We’ll send you a magic code to reset it!
+        </Text> */}
         <Text style={styles.label}>Enter your email address</Text>
         <TextInput
           style={styles.input}
@@ -60,7 +65,7 @@ const ForgotPasswordScreen = () => {
           autoCapitalize="none"
         />
         <TouchableOpacity style={styles.button} onPress={handleSendOTP}>
-          <Text style={styles.buttonText}>Send OTP</Text>
+          <Text style={styles.buttonText}>Send</Text>
         </TouchableOpacity>
       </ScrollView>
     </KeyboardAvoidingView>
@@ -70,64 +75,64 @@ const ForgotPasswordScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f1e7fe',
+    backgroundColor: "#f1e7fe",
   },
   scrollContainer: {
     flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
   },
   logo: {
     width: 200,
     height: 200,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     marginBottom: 40,
   },
   title: {
     fontSize: 28,
-    fontWeight: 'bold',
-    color: '#6a1b9a',
-    textAlign: 'center',
+    fontWeight: "bold",
+    color: "#6a1b9a",
+    textAlign: "center",
     marginBottom: 10,
   },
   subtitle: {
     fontSize: 16,
-    color: '#555',
-    textAlign: 'center',
+    color: "#555",
+    textAlign: "center",
     marginBottom: 20,
   },
   label: {
     fontSize: 16,
-    color: '#333',
+    color: "#333",
     marginBottom: 8,
   },
   input: {
     height: 50,
-    width: '100%',
-    borderColor: '#ddd',
+    width: "100%",
+    borderColor: "#ddd",
     borderWidth: 1,
     borderRadius: 10,
     paddingHorizontal: 15,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     marginBottom: 20,
   },
   button: {
-    backgroundColor: '#6a1b9a',
+    backgroundColor: "#6a1b9a",
     paddingVertical: 15,
     paddingHorizontal: 30,
     borderRadius: 10,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 5,
-    alignItems: 'center',
-    width: '100%',
+    alignItems: "center",
+    width: "100%",
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
-    fontWeight: '600',
+    fontWeight: "600",
   },
 });
 
